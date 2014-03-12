8fa0438a725c0417ac9b6024c7a3591ac9b8681b
Teodor Heggelund <teodor.heggelund@gmail.com>
2014-03-12 18:38:00 +0100

    Prepared code for presentation

--- tkt4116/exercise8.m
+++ tkt4116/exercise8.m
@@ -8,28 +8,150 @@ R = trapz(x, q);
 Mr = trapz(x, q.*x);
 L = x(end);
 
-% By * L = Mr --> By = Mr/L
-By = Mr/L
-Ay = R - By
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
+% WOOO#1
 
-% b)
-n = length(x);
-V = zeros(1,n);
-M = zeros(1,n);
+% By * L = Mr --> By = Mr/L
+% By = Mr/L
+% Ay = R - By
+
+% % b)
+% n = length(x);
+% V = zeros(1,n);
+% M = zeros(1,n);
+
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
 
 V(1) = Ay;
 M(1) = 0;
 
-for i = 1 : n-1	% Regner ut verdier for i+1. Maks: i + 1 = n --> i = n-1
-	dx = x(i+1) - x(i);
-	q_avg = (q(i) + q(i))/2;
-	V(i + 1) = V(i) - q_avg * dx;
-	M(i + 1) = M(i) + V(i) * dx - q_avg * dx^2/2;
-end
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
+% WOOO#2
 
-fprintf('The maximum value of M is %.2f kNm\n', max(M));
+% for i = 1 : n-1	% Regner ut verdier for i+1. Maks: i_max + 1 = n --> i_max = n-1
+% 	dx = x(i+1) - x(i);
+% 	q_avg = (q(i) + q(i))/2;
+% 	V(i + 1) = V(i) - q_avg * dx;
+% 	M(i + 1) = M(i) + V(i) * dx - q_avg * dx^2/2;
+% end
 
-figure('name', 'V-diagram');
-plot(x, V)
-figure('name', 'M-diagram');
-plot(x, M)
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+% WOOO#3
+
+% fprintf('The maximum value of M is %.2f kNm\n', max(M));
+
+% figure('name', 'V-diagram');
+% plot(x, V)
+% figure('name', 'M-diagram');
+% plot(x, M)
