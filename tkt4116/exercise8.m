815781b4d9e5152c02611ef4519c772ae8a52d0f
Teodor Heggelund <teodor.heggelund@gmail.com>
2014-03-13 16:08:57 +0100

    Removed comments not needed

--- tkt4116/exercise8.m
+++ tkt4116/exercise8.m
@@ -8,36 +8,6 @@ R = trapz(x, q);
 Mr = trapz(x, q.*x);
 L = x(end);
 
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-% WOOO#1
-
 % By * L = Mr --> By = Mr/L
 % By = Mr/L
 % Ay = R - By
@@ -47,68 +17,8 @@ L = x(end);
 % V = zeros(1,n);
 % M = zeros(1,n);
 
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-
 V(1) = Ay;
 M(1) = 0;
-
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
-% WOOO#2
 
 % for i = 1 : n-1	% Regner ut verdier for i+1. Maks: i_max + 1 = n --> i_max = n-1
 % 	dx = x(i+1) - x(i);
@@ -116,38 +26,6 @@ M(1) = 0;
 % 	V(i + 1) = V(i) - q_avg * dx;
 % 	M(i + 1) = M(i) + V(i) * dx - q_avg * dx^2/2;
 % end
-
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
-% WOOO#3
 
 % fprintf('The maximum value of M is %.2f kNm\n', max(M));
 
