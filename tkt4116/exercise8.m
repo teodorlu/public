728f090987f800a3e4592918df355d278e04d5a5
Teodor Heggelund <teodor.heggelund@gmail.com>
2014-03-14 09:01:25 +0100

    Uncommented lines

--- tkt4116/exercise8.m
+++ tkt4116/exercise8.m
@@ -8,28 +8,28 @@ R = trapz(x, q);
 Mr = trapz(x, q.*x);
 L = x(end);
 
-% By * L = Mr --> By = Mr/L
-% By = Mr/L
-% Ay = R - By
+By * L = Mr --> By = Mr/L
+By = Mr/L
+Ay = R - By
 
-% % b)
-% n = length(x);
-% V = zeros(1,n);
-% M = zeros(1,n);
+% b)cd 
+n = length(x);
+V = zeros(1,n);
+M = zeros(1,n);
 
 V(1) = Ay;
 M(1) = 0;
 
-% for i = 1 : n-1	% Regner ut verdier for i+1. Maks: i_max + 1 = n --> i_max = n-1
-% 	dx = x(i+1) - x(i);
-% 	q_avg = (q(i) + q(i))/2;
-% 	V(i + 1) = V(i) - q_avg * dx;
-% 	M(i + 1) = M(i) + V(i) * dx - q_avg * dx^2/2;
-% end
+for i = 1 : n-1	% Regner ut verdier for i+1. Maks: i_max + 1 = n --> i_max = n-1
+	dx = x(i+1) - x(i);
+	q_avg = (q(i) + q(i))/2;
+	V(i + 1) = V(i) - q_avg * dx;
+	M(i + 1) = M(i) + V(i) * dx - q_avg * dx^2/2;
+end
 
-% fprintf('The maximum value of M is %.2f kNm\n', max(M));
+fprintf('The maximum value of M is %.2f kNm\n', max(M));
 
-% figure('name', 'V-diagram');
-% plot(x, V)
-% figure('name', 'M-diagram');
-% plot(x, M)
+figure('name', 'V-diagram');
+plot(x, V)
+figure('name', 'M-diagram');
+plot(x, M)
